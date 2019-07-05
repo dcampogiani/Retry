0085d5fc9c250ae8a0ffb44034b3e4d326096886
Daniele Campogiani <daniele.campogiani@gmail.com>
2019-07-05 14:35:40 +0200

    Add launchWithRetry

--- app/src/main/java/com/danielecampogiani/retry/ViewModelExtensions.kt
+++ app/src/main/java/com/danielecampogiani/retry/ViewModelExtensions.kt
@@ -1,2 +1,33 @@
 package com.danielecampogiani.retry
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.danielecampogiani.retry.connection.ConnectionChecker
+import kotlinx.coroutines.launch
+
+fun <T> ViewModel.launchWithRetry(
+    networkOperation: suspend () -> T,
+    loading: () -> Unit,
+    resultConsumer: (T) -> Unit,
+    onNoNetwork: () -> Unit,
+    connectionChecker: ConnectionChecker
+) {
+
+    if (connectionChecker.isConnected) {
+        loading()
+        viewModelScope.launch {
+            val networkData = networkOperation()
+            resultConsumer(networkData)
+        }
+    } else {
+        onNoNetwork()
+        val listener = object : ConnectionChecker.Listener {
+            override fun onConnected() {
+                connectionChecker.removeListener(this)
+                launchWithRetry(networkOperation, loading, resultConsumer, onNoNetwork, connectionChecker)
+            }
+        }
+        connectionChecker.addListener(listener)
+    }
+
+}
